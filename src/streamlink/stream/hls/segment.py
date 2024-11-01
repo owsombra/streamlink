bf3ccc3b73f9dbd30cb408a6e3fd6f0ec1d5fbc4
Hyeonwoo Lee <hw.lee@draftify.gg>
2024-11-01 18:58:13 +0900

    Fix: conflicts

--- src/streamlink/stream/hls/segment.py
+++ src/streamlink/stream/hls/segment.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 from datetime import datetime, timedelta
-from typing import NamedTuple
+from typing import NamedTuple, Optional
 
 from streamlink.stream.segmented.segment import Segment
 
